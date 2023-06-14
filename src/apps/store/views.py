0826b3954ccd41316cb339ef81626b4bfa60a947
Rafael Maia <rafaeucaetanoquintas@gmail.com>
2023-06-14 10:40:07 -0300

    Added ContextMixin and Shipping

--- src/apps/store/views.py
+++ src/apps/store/views.py
@@ -1,50 +1,70 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+import json
 
-from apps.store.models import *
+from django.http import JsonResponse
+from django.views.generic import TemplateView, ListView
+from django.views.generic.base import ContextMixin
+
+from apps.store.models import Product, Order, OrderItem
 
 
 # Create your views here.
 
-class StoreList(ListView):
+class CartContextMixin(ContextMixin):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            customer = self.request.user.customer
+            order, created = Order.objects.get_or_create(customer=customer, complete=False)
+            items = order.orderitem_set.all()
+            cart_items = order.cart_items
+        else:
+            items = []
+            order = {'cart_total': 0, 'cart_items': 0, 'shipping': False}
+            cart_items = order['cart_items']
+
+        context['items'] = items
+        context['order'] = order
+        context['cart_items'] = cart_items
+        return context
+
+
+class StoreList(CartContextMixin, ListView):
     model = Product
     template_name = 'store/store.html'
 
 
-class CartView(TemplateView):
+class CartView(CartContextMixin, TemplateView):
     template_name = 'store/cart.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
 
-        if self.request.user.is_authenticated:
-            customer = self.request.user.customer
-            order, created = Order.objects.get_or_create(customer=customer, complete=False)
-            items = order.orderitem_set.all()
-        else:
-            items = []
-            order = {'get_cart_total':0, 'get_cart_items':0}
-
-        context['items'] = items
-        context['order'] = order
-        return context
-
-
-class CheckoutView(TemplateView):
+class CheckoutView(CartContextMixin, TemplateView):
     template_name = 'store/checkout.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
 
-        if self.request.user.is_authenticated:
-            customer = self.request.user.customer
-            order, created = Order.objects.get_or_create(customer=customer, complete=False)
-            items = order.orderitem_set.all()
-        else:
-            items = []
-            order = {'get_cart_total':0, 'get_cart_items':0}
+def updateitem(request):
+    data = json.loads(request.body)
+    product_id = data['productId']
+    action = data['action']
 
-        context['items'] = items
-        context['order'] = order
-        return context
+    print('action', action)
+    print('productId', product_id)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=product_id)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    order_item, created, = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        order_item.quantity = (order_item.quantity + 1)
+    elif action == 'remove':
+        order_item.quantity = (order_item.quantity - 1)
+
+    order_item.save()
+
+    if order_item.quantity <= 0:
+        order_item.delete()
+
+    return JsonResponse('Item was added', safe=False)
